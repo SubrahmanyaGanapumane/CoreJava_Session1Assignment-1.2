713357565594ff70097c57ce4ebdcc1c12d079c7
SubrahmanyaGanapumane <subrahmanyagg@gmail.com>
2016-12-24 12:02:17 +0530

    Add files via upload

---
+++ Assignment_2.java
@@ -0,0 +1,15 @@
+package Session1;
+
+public class Assignment_2 {
+
+	public static void main(String args[])
+	{
+		int a=20;
+		int b= a-- - --a;
+		int c=a--;
+		int d=a>>2;
+		int e=a&b;
+		System.out.println("Values are b="+b+" c="+c+" d="+d+ " e="+e);
+
+	}
+}

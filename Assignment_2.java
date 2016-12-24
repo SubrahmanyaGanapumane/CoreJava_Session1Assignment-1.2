4bb7c5295191872dab3e50c9f03be8acecde9ea2
SubrahmanyaGanapumane <subrahmanyagg@gmail.com>
2016-12-24 12:07:22 +0530

    Update Assignment_2.java

--- Assignment_2.java
+++ Assignment_2.java
@@ -1,5 +1,8 @@
 package Session1;
 
+//(if e to affected with original b value then calculate the e=a&b first and rest later)
+
+
 public class Assignment_2 {
 
 	public static void main(String args[])
@@ -9,6 +12,24 @@ public class Assignment_2 {
 		int c=a--;
 		int d=a>>2;
 		int e=a&b;
+		System.out.println("Values are b="+b+" c="+c+" d="+d+ " e="+e);
+
+	}
+}
+// other virsion is below
+/*
+
+
+public class Assignment_2 {
+
+	public static void main(String args[])
+	{
+		int e=a&b;
+		int a=20;
+		int b= a-- - --a;
+		int c=a--;
+		int d=a>>2;
+		
 		System.out.println("Values are b="+b+" c="+c+" d="+d+ " e="+e);
 
 	}
